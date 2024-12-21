2cfffc37d1efe83d0d3076403622b0718e73466a
zhoumeng <zhoumeng@roborock.com>
2024-12-21 14:11:35 +0800

    fix toolchain.cmake;

--- cmake/toolchain.cmake
+++ cmake/toolchain.cmake
@@ -1,10 +1,18 @@
-# 指定交叉编译器
+# 设置交叉编译的操作系统和架构
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR arm)
 
-# 交叉编译工具链路径
-set(TOOLCHAIN_PATH /path/to/cross-compiler)
+# 设置交叉编译工具链的路径
+set(CMAKE_C_COMPILER /opt/toolchain/aarch64-mr527-linux-gnu/bin/aarch64-mr527-linux-gnu-gcc)
+set(CMAKE_CXX_COMPILER /opt/toolchain/aarch64-mr527-linux-gnu/bin/aarch64-mr527-linux-gnu-g++)
 
-# 指定交叉编译器
-set(CMAKE_C_COMPILER ${TOOLCHAIN_PATH}/bin/arm-linux-gnueabi-gcc)
-set(CMAKE_CXX_COMPILER ${TOOLCHAIN_PATH}/bin/arm-linux-gnueabi-g++)
+# 设置目标平台的 sysroot（可选，如果有的话）
+set(CMAKE_SYSROOT /opt/toolchain/aarch64-mr527-linux-gnu/aarch64-mr527-linux-gnu/sysroot/)
+
+# 设置交叉编译工具链的查找路径
+set(CMAKE_FIND_ROOT_PATH /opt/toolchain/aarch64-mr527-linux-gnu)
+
+# 设置查找程序、库和头文件时的查找模式
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
