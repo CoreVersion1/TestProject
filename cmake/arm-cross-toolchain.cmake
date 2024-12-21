4bfbd567bebace507aa2b4b35ee8cc629c8f2088
zhoumeng <zhoumeng@roborock.com>
2024-12-21 16:25:23 +0800

    update dir;

--- cmake/arm-cross-toolchain.cmake
+++ cmake/arm-cross-toolchain.cmake
@@ -2,10 +2,6 @@
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR arm)
 
-# 设置交叉编译工具链的路径
-set(CMAKE_C_COMPILER /opt/toolchain/aarch64-mr527-linux-gnu/bin/aarch64-mr527-linux-gnu-gcc)
-set(CMAKE_CXX_COMPILER /opt/toolchain/aarch64-mr527-linux-gnu/bin/aarch64-mr527-linux-gnu-g++)
-
 # 设置目标平台的 sysroot（可选，如果有的话）
 set(CMAKE_SYSROOT /opt/toolchain/aarch64-mr527-linux-gnu/aarch64-mr527-linux-gnu/sysroot/)
 
@@ -16,3 +12,7 @@ set(CMAKE_FIND_ROOT_PATH /opt/toolchain/aarch64-mr527-linux-gnu)
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
+# 设置交叉编译工具链的路径
+set(CMAKE_C_COMPILER /opt/toolchain/aarch64-mr527-linux-gnu/bin/aarch64-mr527-linux-gnu-gcc)
+set(CMAKE_CXX_COMPILER /opt/toolchain/aarch64-mr527-linux-gnu/bin/aarch64-mr527-linux-gnu-g++)
