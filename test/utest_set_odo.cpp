49511db72ed7207a7dd45c99f029fc6a3dfeff08
zhoumeng <zhoumeng@roborock.com>
2024-12-31 16:10:26 +0800

    add get odo;

---
+++ test/utest_set_odo.cpp
@@ -0,0 +1,23 @@
+#include <chrono>
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+#include <thread>
+
+#include "roborock/rda_headers.h"
+
+int main() {
+  auto ret = init_uart();
+  if (ret != 0) {
+    std::cout << "init_uart failed" << std::endl;
+    return -1;
+  }
+
+  clean_uart();
+  std::this_thread::sleep_for(std::chrono::seconds(1));
+
+  McuOdoSpd_t spd_info = {1,2};
+  ret = sent_basic_cmd(CMD_SET_ODO_SPD_ID, sizeof(spd_info), &spd_info);
+
+  return 0;
+}
