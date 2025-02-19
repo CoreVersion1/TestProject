c5d95d8fc8ee78a95a1f80a226870ff4ce431539
zhoumeng <zhoumeng@roborock.com>
2025-02-19 20:56:04 +0800

    add class TimeImpl;

---
+++ inc/Common/TimeImpl.hpp
@@ -0,0 +1,70 @@
+#include <string>
+
+#pragma once
+
+namespace TestProject {
+
+class TimeImpl {
+ public:
+  /**
+   * @brief get current time
+   *
+   * @return int64_t
+   */
+  int64_t Now();
+
+  /**
+   * @brief get current time string
+   *
+   * @return std::string
+   */
+  std::string NowStr();
+
+  /**
+   * @brief set time record
+   *
+   * @param time_ms
+   */
+  void SetTimeRecord(int64_t time_ms);
+
+  /**
+   * @brief get time record
+   *
+   * @return int64_t
+   */
+  int64_t GetTimeRecord();
+
+  /**
+   * @brief clear time record
+   *
+   */
+  void ClearTimeRecord(void);
+
+  /**
+   * @brief get freq
+   *
+   * @param last_time_ms
+   * @param now_time_ms
+   * @return float
+   */
+  float GetFreq(int64_t last_time_ms, int64_t now_time_ms);
+
+  /**
+   * @brief get freq
+   *
+   * @param time_ms
+   * @return float
+   */
+  float GetFreq(int64_t time_ms);
+
+  /**
+   * @brief get freq
+   *
+   * @return float
+   */
+  float GetFreq();
+
+ private:
+  int64_t last_time_ = 0;
+};
+}  // namespace TestProject
---
+++ src/Common/TimeImpl.cpp
@@ -0,0 +1,67 @@
+#include "Common/TimeImpl.hpp"
+
+#include <chrono>
+#include <ctime>
+#include <sstream>
+#include <iomanip>
+#include <stdexcept>
+
+namespace TestProject {
+
+int64_t TimeImpl::Now()
+{
+  using namespace std::chrono;
+  return duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
+}
+
+std::string TimeImpl::NowStr()
+{
+  using namespace std::chrono;
+  auto now             = system_clock::now();
+  std::time_t now_time = system_clock::to_time_t(now);
+  std::tm tm           = *std::localtime(&now_time);
+
+  std::stringstream ss;
+  ss << std::put_time(&tm, "%Y-%m-%d %H:%M:%S");
+  return ss.str();
+}
+
+void TimeImpl::SetTimeRecord(int64_t time_ms)
+{
+  last_time_ = time_ms;
+}
+
+int64_t TimeImpl::GetTimeRecord()
+{
+  return last_time_;
+}
+
+void TimeImpl::ClearTimeRecord()
+{
+  last_time_ = 0;
+}
+
+float TimeImpl::GetFreq(int64_t last_time_ms, int64_t now_time_ms)
+{
+  if (now_time_ms <= last_time_ms)
+  {
+    throw std::invalid_argument("Current time must be greater than last time.");
+  }
+
+  // Frequency in Hz (1 / time difference in seconds)
+  return 1000.0f / (now_time_ms - last_time_ms);
+}
+
+float TimeImpl::GetFreq(int64_t now_time_ms)
+{
+  auto last_time_ms = last_time_;
+  last_time_        = now_time_ms;
+  return GetFreq(last_time_ms, now_time_ms);
+}
+
+float TimeImpl::GetFreq()
+{
+  int64_t now_time_ms = Now();
+  return GetFreq(now_time_ms);
+}
+}  // namespace TestProject
